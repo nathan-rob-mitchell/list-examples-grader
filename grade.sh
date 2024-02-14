2f51bdfcfeb525949421451e650a1ae407b0eb81
nathan-rob-mitchell <nrmitchell@ucsd.edu>
2024-02-14 13:40:52 -0800

    update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,35 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+cp student-submission/*.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+pwd
+cd grading-area
+pwd
+
+if  ! [ -f ListExamples.java ]
+then 
+    echo "Missing: ListExamples.java in student submission"
+    echo "Score: 0"
+    exit
+fi 
+
+javac -cp $CPATH *.java &> compile.txt
+if [ $? -ne 0 ]
+then 
+    echo "Compilation error"
+    echo "Score: 0"
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples &> junit-output.txt
+
+lastline=$(cat junit-output.txt | tail -n 1)
+tests=$(echo  $lastline | awk -F'[, ]' '{print $3}')
+failures=$(echo  $lastline | awk -F'[, ]' '{print $6}')
+successes=$((tests - failures))
+
+echo "Your score is $successes / $tests"
